4d255c598a7e7d04846808b69e9c02b81da40539
Parsa Poorsistani <poorsistani13@gmail.com>
2023-04-24 01:03:50 +0330

    q2 done

---
+++ Employee-Management/manager.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+if [ "$1" = "city" ]; then
+    employees=$(grep -i "$2" employee.csv)
+    if [ -z "$employees" ]; then
+        echo "no employees found"
+    
+    else
+        while read -r line; do
+        name=$(echo "$line" | cut -d ',' -f 3)
+        echo "Customer Name: "$name""
+        number=$(echo "$line" | cut -d ',' -f 4)
+        echo "Mobile No: $number"
+        done <<< "$employees"
+    fi
+elif [ "$1" = "bonus" ]; then
+    employee=$(grep -i $2 employee.csv)
+    salary=$(echo $employee | cut -d ',' -f 5)
+    name=$(echo $employee | cut -d ',' -f 3)
+    bonus=$(echo "$salary * 0.05 / 1" | bc)
+    echo ""$name" will get \$$bonus bonus"
+else
+    echo "command not found"
+    exit 1
+fi
