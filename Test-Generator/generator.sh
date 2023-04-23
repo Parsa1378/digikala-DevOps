e6a02b9cc8b4ca4176e76684eb9eb464727d7c23
Parsa Poorsistani <poorsistani13@gmail.com>
2023-04-23 23:03:07 +0330

    test generator done

--- Test-Generator/generator.sh
+++ Test-Generator/generator.sh
@@ -1,1 +1,16 @@
-#!bin/bash
+#!/bin/bash
+mkdir -p out
+# Loop through the test cases and process them
+for i in $(seq 1 $1); do
+  input_file="in/input${i}.txt"
+  output_file="out/output${i}.txt"
+
+  # Check if the input file exists
+  if [ ! -f "$input_file" ]; then
+    echo "Error: Input file '${input_file}' not found."
+    exit 1
+  fi
+
+  # Run the main.py script with the input file and save the output to the output file
+  python3 main.py < "$input_file" > "$output_file"
+done
